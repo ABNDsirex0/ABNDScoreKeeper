348b5ff81c276ad11a4214cd05e666d4e6b94eca
Krzysztof Kin <sirex0@gmail.com>
2017-11-28 14:35:14 +0100

    Comments for code

--- app/src/main/java/com/example/android/ngscorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/ngscorekeeper/MainActivity.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Variables to keep score. A,B - teams
     int scoreA = 0;
     int scoreB = 0;
     int cardYellowA = 0;
@@ -18,77 +20,129 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Display initial values on layout
         displayScoreA();
         displayScoreB();
         displayYellowA();
         displayYellowB();
         displayRedA();
         displayRedB();
-
-        //displayRedScore();
     }
 
+    /**
+     * Display sore for team A
+     */
     public void displayScoreA() {
         TextView scoreView = (TextView) findViewById(R.id.text_score_a);
         scoreView.setText(String.valueOf(scoreA));
     }
 
+    /**
+     * Display score for team B
+     */
     public void displayScoreB() {
         TextView scoreView = (TextView) findViewById(R.id.text_score_b);
         scoreView.setText(String.valueOf(scoreB));
     }
 
+    /**
+     * Display counter for team A yellow cards
+     */
     public void displayYellowA() {
         TextView scoreView = (TextView) findViewById(R.id.yellow_score_a);
         scoreView.setText(String.valueOf(cardYellowA));
     }
 
+    /**
+     * Display counter for team B yellow cards
+     */
     public void displayYellowB() {
         TextView scoreView = (TextView) findViewById(R.id.yellow_score_b);
         scoreView.setText(String.valueOf(cardYellowB));
     }
 
+    /**
+     * Display counter for team B yellow cards
+     */
     public void displayRedA() {
         TextView scoreView = (TextView) findViewById(R.id.red_score_a);
         scoreView.setText(String.valueOf(cardRedA));
     }
 
+    /**
+     * Display counter for team B yellow cards
+     */
     public void displayRedB() {
         TextView scoreView = (TextView) findViewById(R.id.red_score_b);
         scoreView.setText(String.valueOf(cardRedB));
     }
 
+    /**
+     * When goal button for team A is pressed
+     *
+     * @param view view
+     */
     public void clickGoalA(View view) {
         scoreA += 1;
         displayScoreA();
     }
 
+    /**
+     * When goal button for team B is pressed
+     *
+     * @param view view
+     */
     public void clickGoalB(View view) {
         scoreB += 1;
         displayScoreB();
     }
 
+    /**
+     * When yellow card button for team A is pressed
+     *
+     * @param view view
+     */
     public void clickYellowA(View view) {
         cardYellowA += 1;
         displayYellowA();
     }
 
+    /**
+     * When yellow card button for team B is pressed
+     *
+     * @param view view
+     */
     public void clickYellowB(View view) {
         cardYellowB += 1;
         displayYellowB();
     }
 
-
+    /**
+     * When red card button for team A is pressed
+     *
+     * @param view view
+     */
     public void clickRedA(View view) {
         cardRedA += 1;
         displayRedA();
     }
 
+    /**
+     * When red card button for team B is pressed
+     *
+     * @param view view
+     */
     public void clickRedB(View view) {
         cardRedB += 1;
         displayRedB();
     }
 
+    /**
+     * When click reset button
+     *
+     * @param view view
+     */
     public void clickReset(View view) {
         scoreB = 0;
         scoreA = 0;
